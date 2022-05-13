d1d5d0bc505ec264accbcd69e2e0b356d5566366
Pleasure GT <pleasuregt01@gmail.com>
2022-05-13 16:40:51 -0400

    Added the form with the email wokring, need to look better

--- public/index.js
+++ public/index.js
@@ -53,3 +53,26 @@ function getCookie(name) {
 if (getCookie("isDark") == "true") {
     toggleMode();
 }
+
+// FORM emailJs
+
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('erahkk0th3Z3BKcn9');
+})();
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('service_mksrijc', 'template_y5kbr8g', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+        
+    });
+}
