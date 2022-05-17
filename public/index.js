12ebd2f3c50d8657e6f4b495ed9556f217edd159
Pleasure GT <pleasuregt01@gmail.com>
2022-05-16 22:07:11 -0400

    Cookie but was not good, kept for extra cookie, styling about, home, done

--- public/index.js
+++ public/index.js
@@ -1,6 +1,10 @@
 const darkMode = document.querySelector("#dark-mode");
 const modeLogo = document.querySelector("#mode-logo");
 const modeText = document.querySelector("#mode-text");
+const cookieStyle = document.querySelector(".simple-cookie-consent");
+const cookieModel = document.querySelector(".cookie-consent-container")
+const cancelButton = document.querySelector(".cookie-consent-deny")
+const acceptButton = document.querySelector(".cookie-consent-allow");
 
 function slide() {
     let menu = document.getElementById("menu");
@@ -56,11 +60,22 @@ if (getCookie("isDark") == "true") {
 
 // FORM emailJs
 
+
+/***
+ * Made to send mails through a server 
+ * This makes it easier since it will have template to send mails
+ * 
+ */
 (function() {
     // https://dashboard.emailjs.com/admin/account
     emailjs.init('erahkk0th3Z3BKcn9');
 })();
 
+/**
+ * Listens for the submit, and then sends a server request for it to be sent to the 
+ * email address provided
+ * 
+ */
 window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
@@ -93,4 +108,64 @@ window.onload = function() {
         }
         
     });
+}
+
+/**
+ * Set the parameter to the cookie name, the value, the number of days to expire
+ * 
+ * @param {*} cname 
+ * @param {*} cvalue 
+ * @param {*} exdays 
+ */
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+/**
+ * Delete the cookie with the name passed as parameter
+ * @param {*} cname 
+ */
+function deleteCookie(cname) {
+    const d = new Date();
+    d.setTime(d.getTime() + (24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=;" + expires + ";path=/";
+}
+
+/**
+ * Check if the cookie is set or not, if not then show the cookie consent
+ * @param {*} cname 
+ * @returns nothing
+ */
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+/**
+ * Check if the cookie is set or not, if not then show the cookie consent
+ */
+function acceptCookieConsent(){
+    deleteCookie('user_cookie_consent');
+    setCookie('user_cookie_consent', 1, 30);
+    document.getElementById("cookieNotice").style.display = "none";
+}
+
+let cookie_consent = getCookie("user_cookie_consent");
+if(cookie_consent != ""){
+    document.getElementById("cookieNotice").style.display = "none";
+}else{
+    document.getElementById("cookieNotice").style.display = "block";
 }
