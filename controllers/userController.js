34aa2363dd204cd5ed47c10dd74aef5083111cef
LiamKopke <78220160+LiamKopke@users.noreply.github.com>
2022-05-19 13:57:19 -0400

    fixed breaking on no available items for rental

--- controllers/userController.js
+++ controllers/userController.js
@@ -101,49 +101,97 @@ router.get('/rent', async function (request, response) {
 });
 
 router.post('/rentSubmit', async function (request, response) {
-        let startTime = request.body.startTime;
-        let duration = request.body.duration;
-        let itemType = request.body.itemType;  
-        let endTime = "";
+    const pageData = {
+        rent: true,
+        itemsSnow: 
+        [
+            {               
+                name: "Snowboard",
+                formInput: "/rentSubmit",
+                image: "/images/snowboardMain.jpg",
+                itemType: "Snowboards",
+            },
+            {
+                name: "Snowboard Boots",
+                formInput: "/rentSubmit",
+                image: "/images/snowbardBoots.png",
+                itemType: "Boots"
+            }
+        ],
+        items: [
+            
+            {
+                name: "Ski",
+                formInput: "/rentSubmit",
+                image: "/images/skiRent.png",
+                itemType: "Skis"
+            },
+            {
+                name: "Helmet",
+                formInput: "/rentSubmit",
+                image: "/images/helmet.png",
+                itemType: "Helmets"
+            },
+            {
+                name: "Ski Boots",
+                formInput: "/rentSubmit",
+                image: "/images/skiBoots.png",
+                itemType: "Boots"
+            },
+            {
+                name: "Poles",
+                formInput: "/rentSubmit",
+                image: "/images/poles.png",
+                itemType: "Poles"
+            },
+        
+        ]
+    };
 
-        if(!await model.authenticateUser(request)){
-            response.render("login.hbs", {message: "Unauthorized Access - Please log in to an account to use this feature"}); 
+    let startTime = request.body.startTime;
+    let duration = request.body.duration;
+    let itemType = request.body.itemType;  
+    let endTime = "";
+
+    if(!await model.authenticateUser(request)){
+        response.render("login.hbs", {message: "Unauthorized Access - Please log in to an account to use this feature"}); 
+    }
+    else
+    {
+        const session = await model.refreshSession(request, response);
+        const expiresAt = new Date(session.expiresAt);
+        response.cookie("sessionId", session.id, { expires: expiresAt });
+        response.cookie("userId", session.userId, { expires: expiresAt });
+        response.cookie("userType", session.userType, { expires: expiresAt });       
+        
+        
+        // Make sure variables are correct time
+        try{
+            startTime = new Date(startTime);
+            duration = parseInt(duration);
+            endTime = new Date(addTime(startTime, {hours: duration}));
         }
-        else
-        {
-            const session = await model.refreshSession(request, response);
-            const expiresAt = new Date(session.expiresAt);
-            response.cookie("sessionId", session.id, { expires: expiresAt });
-            response.cookie("userId", session.userId, { expires: expiresAt });
-            response.cookie("userType", session.userType, { expires: expiresAt });       
-            
-            
-            // Make sure variables are correct time
-            try{
-                startTime = new Date(startTime);
-                duration = parseInt(duration);
-                endTime = new Date(addTime(startTime, {hours: duration}));
-            }
-            catch(error){
-                console.error(error.message);
-            }
+        catch(error){
+            console.error(error.message);
+        }
 
-            // Try to create the rental
-            try {
-                // Create the rental
-                await model.createRental(session.userId, startTime, endTime, duration, itemType);
-                console.log("Successfully rented ski equipment");
-                
-                // TODO, change this to account so that you can see ur rental
-                response.render("home.hbs", {message: "Successfully rented ski equipment"});
-            }
-            catch (err) {
-                // If it didnt work, display error message and return to rental page
-                console.error(err.message);            
-                response.render('/rent', {message: err.message});
-            }
+        // Try to create the rental
+        try {
+            // Create the rental
+            await model.createRental(session.userId, startTime, endTime, duration, itemType);
+            console.log("Successfully rented ski equipment");
+            
+            // TODO, change this to account so that you can see ur rental
+            response.render("home.hbs", {message: "Successfully rented ski equipment"});
+        }
+        catch (err) {
+            // If it didnt work, display error message and return to rental page
+            console.error(err.message);     
+            pageData.message = err.message;      
+            response.render('rent.hbs', pageData);
         }
     }
+}
 );
 
 router.get('/about', about);
